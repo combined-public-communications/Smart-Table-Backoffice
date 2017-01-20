051dabb4bcc12c4e9d8f5cb7c06783c6410b57e1
Brad Gardner <bgardner87@gmail.com>
2017-01-20 09:09:53 -0500

    hotfix for loading override

--- src/stPipe.js
+++ src/stPipe.js
@@ -36,7 +36,7 @@ ng.module('smart-table')
         },
 
         post: function (scope, element, attrs, ctrl) {
-          var pipeOnLoad = attrs.stExecutePipeOnLoad || config.pipe.executePipeOnLoad;
+          var pipeOnLoad = attrs.stExecutePipeOnLoad === "false" ? false : config.pipe.executePipeOnLoad;
           if (pipeOnLoad) {
             ctrl.pipe();
           }
